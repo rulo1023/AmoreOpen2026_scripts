6af58407fc176229431a11f26573defd23963f4e
rulo1023 <rubenlopezdejuan@gmail.com>
2026-02-08 23:49:55 +0100

    Maldita sea, ahora si funciona

--- lib/_unavailabilities.cs
+++ lib/_unavailabilities.cs
@@ -9,10 +9,9 @@ Define("Unavailable",
                 Tuple(2022MART26, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T12:00)]),   # astrid
                 Tuple(2018NAYL01, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T12:00)]),   # gladys
                 Tuple(2025MATA02, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T15:00)]),   # martin garcía
-                Tuple(2023LOIS01, [UnavailableBetween(2026-02-21T15:00, 2026-02-21T23:00)]),   # oscar
-                Tuple(2024RAMO25, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T23:00)]),   # naiara
+                Tuple(2023LOIS01, [UnavailableBetween(2026-02-21T15:00, 2026-02-21T23:00)]),   # oscar 
                 Tuple(2023GERV01, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T23:00)]),   # carlota
-                Tuple(2023LOIS02, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T23:00)])    #roberto
+                Tuple(2023LOIS02, [UnavailableBetween(2026-02-21T00:00, 2026-02-21T23:00)])    # roberto
                ],
                defaultValue=[])
         )
